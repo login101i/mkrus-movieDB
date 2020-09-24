5b944ead4a97246d985c7e277ea26bf9fcacdf91
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-09-24 23:59:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Search from './components/Search'
+import axios from 'axios'
+import { Results } from './components/Results';
+import Popup from './components/Popup'
+
+
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  })
+  const apiurl = 'http://www.omdbapi.com/?apikey=a63d1e8d'
+
+  const search = e => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then((data) => {
+        let results = data.data.Search
+        console.log(data)
+        console.log(results)
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      })
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    })
+    // cg po co to prevState??
+    console.log(state.s)
+
+  }
+
+  const openPopup = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      // cg / czemu data musi być w {} a wyżej już nie musi
+      let result = data;
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      })
+    })
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <h1>Movie DataBase</h1>
       </header>
+      <main>
+        <Search
+          handleInput={handleInput}
+          search={search}
+        />
+        <Results
+          results={state.results}
+          openPopup={openPopup}
+        />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup
+          selected={state.selected}
+          closePopup={closePopup}
+        /> : false}
+      </main>
     </div>
   );
 }
